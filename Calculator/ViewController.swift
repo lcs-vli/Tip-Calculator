d2e0d549968583e6bd3e27f19def5f653752f781
Victoria Li <VLi@lcs.on.ca>
2019-10-10 11:22:26 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Li, Muchen on 2019-10-10.
+//  Copyright © 2019 Li, Muchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
