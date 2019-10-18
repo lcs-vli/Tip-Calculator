12c4c4a13a7c2f42d9a58475476e934d4b2ca576
Victoria Li <VLi@lcs.on.ca>
2019-10-18 11:24:13 -0400

    stretch goal #2 completed

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,51 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
     //MARK: Properties (Outlets)(constants)
     
     @IBOutlet weak var AmountOfBill: UITextField!
-    
-    @IBOutlet weak var tipPercentage: UITextField!
     
     @IBOutlet weak var sliptBetweenPeople: UITextField!
     
     @IBOutlet weak var tipPerPersonString: UILabel!
     
     @IBOutlet weak var totalTipString: UILabel!
+    
+    var tipPercent = 0.15
+    
     //MARK: Methods (actions)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
     
     }
-
-    @IBAction func calculate(_ sender: Any) {
-        //amount of the bill
-        let AmountText = AmountOfBill.text!
-        let bill = Double(AmountText)!
+    
+    
+    @IBAction func five(_ sender: Any) {
+        tipPercent = 0.05
+        calculate()
+    }
+    
+    @IBAction func ten(_ sender: Any) {
+        tipPercent = 0.10
+        calculate()
+    }
+    
+    @IBAction func fifteen(_ sender: Any) {
+        tipPercent = 0.15
+        calculate()
+    }
+    
+    @IBAction func twenty(_ sender: Any) {
+        tipPercent = 0.20
+        calculate()
+    }
+    
+    func calculate()  {
+        //get user input
+        guard let AmountText = AmountOfBill.text else {
+            return
+        }
+        guard let totalPeopleText = sliptBetweenPeople.text else {
+            return
+        }
         
-        //amount of the tip
-        let tipText = tipPercentage.text!
-        let tipPercent1 = Double(tipText)!
-        let tipPercent = tipPercent1/100
-        
-        //number of people
-        let totalPeopleText = sliptBetweenPeople.text!
-        let totalPeople = Double(totalPeopleText)!
-        
-        //value of the tip in dollars and cents
+       //convert all values into doubl
+        guard let bill = Double(AmountText) else {
+            return
+        }
+        guard let totalPeople = Double(totalPeopleText) else {
+            return
+        }
+       
+        //value of the tip in dollars
         let totalTip = bill * tipPercent
-        let totalTipInCent:Double = totalTip * 100
         
         //value of the tip per person
-        let tipPerPerson = round(totalTipInCent/totalPeople)/100
+        let tipPerPerson = round(totalTip/totalPeople)
         
         totalTipString.text = "$\(totalTip)"
         tipPerPersonString.text = "$\(tipPerPerson)"
     }
-    
     
    
 }
